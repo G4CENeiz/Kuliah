b1844b8e55b686539bc3333a584f40997a9f5ba6
Baihaqi <116696555+G4CENeiz@users.noreply.github.com>
2023-05-18 22:52:28 +0700

    ASD drung marti stack e bos

---
+++ Smt 2/Algoritma dan Struktur Data/Praktikum/Week 12/Double Linked Lists/src/Assignment2/Node.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+public class Node {
+    int data;
+    Node prev, next;
+
+    public Node(Node prev, int data, Node next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+}
