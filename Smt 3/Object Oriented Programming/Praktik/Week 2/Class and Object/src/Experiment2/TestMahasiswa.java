9c5ce8bc4fb819358ebab0c0346ef0af89f3b159
Baihaqi <116696555+G4CENeiz@users.noreply.github.com>
2023-09-11 12:53:08 +0700

    Update TestMahasiswa.java

--- Smt 3/Object Oriented Programming/Praktik/Week 2/Class and Object/src/Experiment2/TestMahasiswa.java
+++ Smt 3/Object Oriented Programming/Praktik/Week 2/Class and Object/src/Experiment2/TestMahasiswa.java
@@ -8,8 +8,19 @@ public class TestMahasiswa {
         mhs1.alamat = "Jl. Vinolia No 1A";
         mhs1.kelas = "1A";
         mhs1.tampilBiodata();
-
+        
         Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.nim = 101;
+        mhs2.nama = "Lestari";
+        mhs2.alamat = "Jl. Vinolia No 1A";
+        mhs2.kelas = "1A";
+        mhs2.tampilBiodata();
+        
         Mahasiswa mhs3 = new Mahasiswa();
+        mhs3.nim = 101;
+        mhs3.nama = "Lestari";
+        mhs3.alamat = "Jl. Vinolia No 1A";
+        mhs3.kelas = "1A";
+        mhs3.tampilBiodata();
     }
 }
